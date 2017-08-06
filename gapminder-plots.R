a245266862f814f51475d4395cf16f2c0a42aad5
Mary-Ellen <maryellen@lockss.org>
2017-08-06 10:48:24 -0700

    use log scale

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -6,4 +6,6 @@ gapminder <- read.csv("data/gapminder-FiveYearData.csv")
 library(ggplot2)
 
 ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
-  geom_point(aes(color = continent))
+  geom_point(aes(color = continent)) +
+  scale_x_log10()
+
