66e010f4f921566bc3ced7128a1abe8ff72be537
Mary-Ellen <maryellen@lockss.org>
2017-08-06 10:43:30 -0700

    Add R file

---
+++ gapminder-plots.R
@@ -0,0 +1,9 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv",
+              "data/gapminder-FiveYearData.csv")
+
+gapminder <- read.csv("data/gapminder-FiveYearData.csv")
+
+library(ggplot2)
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point(aes(color = continent))
