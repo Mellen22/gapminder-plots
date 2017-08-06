aba25a6c3873f4787dd4ed17b9d6c91dc2fe8e80
Mary-Ellen <maryellen@lockss.org>
2017-08-06 12:30:11 -0700

    Add space

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -8,5 +8,5 @@ library(ggplot2)
 ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
   geom_point(aes(color = continent)) +
   scale_x_log10() +
-  geom_smooth(method="lm")
+  geom_smooth(method="lm") 
 
